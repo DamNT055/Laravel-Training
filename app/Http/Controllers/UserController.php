df0d0b18be05bcab0a33f3281463205a4625b4e5
Dam Nguyen <damnguyen.tga@gmail.com>
2024-01-12 18:00:37 +0700

    commit code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,1 +1,13 @@
 <?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\View;
+
+class UserController {
+    public function showProfile(string $id): View {
+        $user = Cache::get('user', $id);
+        return view('index', ['user' => $user]);
+    }
+}
